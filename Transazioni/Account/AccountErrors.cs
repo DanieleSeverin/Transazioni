e6a99e67689f4e80caa9d2d894b574c199c971c6
Daniele Severin <danieleseverin89@gmail.com>
2024-06-05 20:25:57 +0200

    Installation of SonarCloud extensions; Fixed some warnings;

--- Transazioni/Account/AccountErrors.cs
+++ Transazioni/Account/AccountErrors.cs
@@ -4,11 +4,11 @@ namespace Transazioni.Domain.Account;
 
 public static class AccountErrors
 {
-    public static Error AlreadyExists = new(
+    public static readonly Error AlreadyExists = new(
         "Account.AlreadyExists",
         "The account already exists.");
 
-    public static Error NotFound = new(
+    public static readonly Error NotFound = new(
         "Account.NotFound",
         "Account not found.");
 }
--- Transazioni/Account/AccountExtensions.cs
+++ Transazioni/Account/AccountExtensions.cs
@@ -5,12 +5,12 @@ public static class AccountExtensions
     public static Accounts? FindByName(this List<Accounts> List, AccountName AccountName)
     {
         string accountName = AccountName.Value.Trim().ToLower();
-        return List.FirstOrDefault(acc => acc.AccountName.Value.Trim().ToLower() == accountName);
+        return List.Find(acc => acc.AccountName.Value.Trim().ToLower() == accountName);
     }
 
     public static Accounts? FindByName(this List<Accounts> List, string AccountName)
     {
         string accountName = AccountName.Trim().ToLower();
-        return List.FirstOrDefault(acc => acc.AccountName.Value.Trim().ToLower() == accountName);
+        return List.Find(acc => acc.AccountName.Value.Trim().ToLower() == accountName);
     }
 }
