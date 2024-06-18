c40133ce45784fdd548e7033b021f34faa29dc30
DanieleSeverin <danieleseverin89@gmail.com>
2024-06-18 22:08:38 +0200

    bug fixes

--- Transazioni/Tokens/RefreshToken.cs
+++ Transazioni/Tokens/RefreshToken.cs
@@ -7,17 +7,17 @@ public class RefreshToken
     public RefreshTokenId Id { get; init; }
     public string Value { get; init; }
     public DateTime CreatedAt { get; init; }
-    public DateTime ExireAt { get; init; }
+    public DateTime ExpireAt { get; init; }
     public UserId UserId { get; init; }
 
-    public User User { get; init; }
+    public User User { get; init; } = null!;
 
     public RefreshToken(string value, UserId userId)
     {
         Id = RefreshTokenId.New();
         Value = value;
         CreatedAt = DateTime.Now;
-        ExireAt = CreatedAt.AddHours(24);
+        ExpireAt = CreatedAt.AddHours(24);
         UserId = userId;
     }
 
