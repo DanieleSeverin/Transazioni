7e4aef4f254ca5f6f94f9011c9e7f6df90a8688f
DanieleSeverin <danieleseverin89@gmail.com>
2024-09-28 09:29:30 +0200

    Fix SeedData method

--- Transazioni.API/Extensions/SeedDataExtensions.cs
+++ Transazioni.API/Extensions/SeedDataExtensions.cs
@@ -14,9 +14,9 @@ public static class SeedDataExtensions
 
         var DbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-        var users = await DbContext.Set<User>().ToListAsync();
+        var firstUser = await DbContext.Set<User>().FirstOrDefaultAsync();
 
-        if (!users.Any())
+        if (firstUser is null || await DbContext.Set<AccountRules>().AnyAsync())
         {
             return;
         }
@@ -26,67 +26,67 @@ public static class SeedDataExtensions
             new AccountRules(
                 new RuleContains("Satispay"), 
                 new AccountName("Satispay"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("PayPal"),
                 new AccountName("PayPal"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Glovo"),
                 new AccountName("Glovo"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("JustEat"),
                 new AccountName("JustEat"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("AMAZON"),
                 new AccountName("Amazon"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("AMZN"),
                 new AccountName("Amazon"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Vinted"),
                 new AccountName("Vinted"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Addebito canone"),
                 new AccountName("Canone"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("PRELIEVO"),
                 new AccountName("Prelievi"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Stipendio"),
                 new AccountName("Stipendio"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Disposizione - RIF:"),
                 new AccountName("Disposizioni"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("Bonif"),
                 new AccountName("Bonifici"),
-                users[0].Id),
+                firstUser.Id),
             
             new AccountRules(
                 new RuleContains("PAGAMENTO POS"),
                 new AccountName("Pagamenti POS"),
-                users[0].Id)
+                firstUser.Id)
         });
 
         await DbContext.SaveChangesAsync();
